f916f1e34d245dd614b1c51908c400b3106fa3cc
rayan <rayanstive9@gmail.com>
2024-12-17 16:13:33 +0300

    feat(portfolio): Add menu

---
+++ src/components/accueil/Accueil.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Accueil = () => {
+  return (
+    <div className="container">
+      <h1>Accueil</h1>
+      <button className="btn btn-danger">Accueil</button>
+    </div>
+  );
+};
+
+export default Accueil;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <img src="" className="" alt="logo" />
+      <ul>
+        <li>
+          <a href="">Accueil</a>
+        </li>
+        <li>
+          <a href="">Accueil</a>
+        </li>
+        <li>
+          <a href="">Accueil</a>
+        </li>
+        <li>
+          <a href="">Accueil</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
