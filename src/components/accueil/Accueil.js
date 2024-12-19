76e3f0a476162ffc6db413c9c87f9d4cce35f956
rayan <rayanstive9@gmail.com>
2024-12-19 09:54:57 +0300

    feat(portfolio): update logo and icon

--- src/components/accueil/Accueil.js
+++ src/components/accueil/Accueil.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Accueil.css";
 
 const Accueil = () => {
   return (
     <div className="accueil">
-      <h1>Accueil</h1>
+      <div className="left">
+        <h1 className="hello">Hello! Me,</h1>
+        <h1>
+          It's <span>Rayan HAJANANTENAINA</span>
+        </h1>
+      </div>
+      <div className="right"></div>
     </div>
   );
 };
