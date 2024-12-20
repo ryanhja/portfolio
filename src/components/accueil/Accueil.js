69fd62dcba2c41c8de57f9d1afa9bd773f6779ac
rayan <rayanstive9@gmail.com>
2024-12-20 13:17:55 +0300

    feat(portfolio): Add title and poste

--- src/components/accueil/Accueil.js
+++ src/components/accueil/Accueil.js
@@ -6,9 +6,10 @@ const Accueil = () => {
     <div className="accueil">
       <div className="left">
         <h1 className="hello">Hello! Me,</h1>
-        <h1>
+        <h1 className="name">
           It's <span>Rayan HAJANANTENAINA</span>
         </h1>
+        <h1 className="poste">Engineer BigData / Developper Python</h1>
       </div>
       <div className="right"></div>
     </div>
