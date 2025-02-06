c41feb0a9090b10f4f63fb3e3afe482e43b68249
Stive <rhajanantenaina@ingenosya.mg>
2025-02-06 20:34:48 +0300

    feat(portfolio): Update Experience

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -5,6 +5,12 @@ const Experience = () => {
     <div className="experience">
       <div className="experience-list">
         <div className="mt-4 p-5 bg-secondary text-white rounded">
+          <p>Depuis Décembre 2024</p>
+          <p>Engineer BigData & Fullstack</p>
+          <p>Yas Madagascar</p>
+          <p>Antananarivo</p>
+        </div>
+        <div className="mt-4 p-5 bg-secondary text-white rounded">
           <p>Novembre 2023 - Novembre 2024</p>
           <p>Apprenti Engineer BigData & Fullstack</p>
           <p>Yas Madagascar</p>
@@ -27,6 +33,24 @@ const Experience = () => {
           <p>Stagiaire Développeur Fullstack/IA </p>
           <p>InnovAnalyse</p>
           <p>Paris, France</p>
+        </div>
+        <div className="mt-4 p-5 bg-secondary text-white rounded">
+          <p>Mai 2021 - Octobre 2021</p>
+          <p>Stagiaire Développeur Fullstack</p>
+          <p>Kairos</p>
+          <p>Fianarantsoa</p>
+        </div>
+        <div className="mt-4 p-5 bg-secondary text-white rounded">
+          <p>Décembre 2019 - Mars 2020</p>
+          <p>Stagiaire Développeur Informatique</p>
+          <p>CENI</p>
+          <p>Antsirabe</p>
+        </div>
+        <div className="mt-4 p-5 bg-secondary text-white rounded">
+          <p>Octobre 2018 - Février 2019</p>
+          <p>Stagiaire Développeur Informatique</p>
+          <p>CENI</p>
+          <p>Antsirabe</p>
         </div>
       </div>
     </div>
